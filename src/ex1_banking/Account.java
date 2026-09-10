aa72b865b8b067c984ddc2bd2a31cecfe95d7177
M <kyjiw@duck.com>
2026-09-11 01:18:59 +0200

    Created roadmap for ex1, new currency class file for ex1

---
+++ src/ex1_banking/Account.java
@@ -0,0 +1,14 @@
+package ex1_banking;
+
+public class Account {
+    String name;
+    String surname;
+    float balance = 0;
+    //currency
+
+
+
+    static void createAccount(){
+
+    }
+}
--- src/ex1_banking/Main.java
+++ src/ex1_banking/Main.java
@@ -1,4 +1,13 @@
 package ex1_banking;
+//=========================Banking system========================= 
+//--Bank account:
+//  [ ] Create
+//--Balance
+//  [ ] Deposit
+//  [ ] Withdraw
+//  [ ] Transfer
+//  [ ] Error handling
+
 
 public class Main {
     
