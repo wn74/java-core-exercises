d1c61c30bbf43f62655236390b3724c2da4b888d
M <219915687+wn74@users.noreply.github.com>
2026-09-12 02:06:16 +0200

    complete exercise 1

--- src/ex1_banking/Main.java
+++ src/ex1_banking/Main.java
@@ -3,18 +3,44 @@ package ex1_banking;
 import java.math.BigDecimal;
 //=========================Banking system========================= 
 //--Bank account:
-//  [ ] Create
-//      []Currency enum
-//      []name
-//      []surname
-//      []balance
+//  [x] Create
+//      [x]Currency enum
+//      [x]owner
+//      [x]balance
 //--Balance
-//  [ ] Deposit
-//  [ ] Withdraw
-//  [ ] Transfer
-//  [ ] Error handling
+//  [x] Deposit
+//  [x] Withdraw
+//  [x] Transfer
+//  [x] Error handling
 
 
 public class Main {
+
+    public static void main(String[] args){
+
+    Account alice = new Account("Alice", Currency.EUR);
+    Account bob = new Account("Bob", Currency.EUR);
+    alice.deposit(new BigDecimal(100));
+    bob.deposit(new BigDecimal(50));
+
+    try{
+        alice.transfer(bob, new BigDecimal(40));
+    } catch (InsufficientFundsException e) {
+
+        System.out.println("Handled error: " + e.getMessage());
+
+    } catch (CurrencyMismatchException e) {
+
+        System.out.println("Handled error: " + e.getMessage());
+
+    }
+
+    System.out.println(bob);
+    System.out.println(alice);
+
+}
+
+
+
     
 }
