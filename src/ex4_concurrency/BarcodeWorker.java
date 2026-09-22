9e7598e815885382b70aa3f5ce56c7e6dc56878f
M <219915687+wn74@users.noreply.github.com>
2026-09-22 06:47:06 +0200

    EX4: Finished

--- src/ex4_concurrency/BarcodeWorker.java
+++ src/ex4_concurrency/BarcodeWorker.java
@@ -16,17 +16,19 @@ public class BarcodeWorker implements Runnable{
         this.workerId = workerId;
 
     }
-
+    //simulates processing time per barcode
     @Override 
     public void run() {
 
         while (true) {
-            
-        
-        //System.out.println("Thread running");
 
         try {
         String barcode = queue.take();
+
+        if (barcode.equals("STOP")) {
+           
+            break;
+        }
         System.out.println("["+ workerId+"] "+"Started processing: " + barcode);
         Thread.sleep(800);
         System.out.println("["+ workerId+"] "+"Finished processing: " + barcode);
--- src/ex4_concurrency/Main.java
+++ src/ex4_concurrency/Main.java
@@ -6,16 +6,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-//====================BARCODE DISPATCHER===============
-//-Operator
-//  [x] Input barcodes from console
-//
-//-Workers
-//  [x] 5 item queue
-//  [x] Working concurrently 
-//
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -30,16 +20,21 @@ public class Main {
         executorService.submit(firstWorker);
         executorService.submit(secondWorker);
 
-
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLine()) {
 
         String input = scanner.nextLine();
+
         try {
             
-            if (input.equalsIgnoreCase("exit")||input.equalsIgnoreCase("quit")){
+            if (input.equalsIgnoreCase("exit")||input.equalsIgnoreCase("quit") || input.equalsIgnoreCase("done")){
+                System.out.println("[Dispatcher] Stopping input. Awaiting remaining queue processing.");
                 scanner.close();
+
+                //2 workers - 2 poison pills 
+                queue.put("STOP");
+                queue.put("STOP");
                 break;
             }
 
@@ -53,12 +48,30 @@ public class Main {
             
         } catch (InterruptedException e) {
             
-            System.out.println("Caught exception: "+ e);
+            System.out.println("Caught Interruptex Exception: "+ e);
             scanner.close();
             break;
         }
             
         }
+
+        while (!queue.isEmpty()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+               
+                System.out.println("Caught Interruptex Exception: "+ e);
+            }
+        }
+
+         
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(1, java.util.concurrent.TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("Caught Interruptex Exception: "+ e);
+        }
+        System.out.println("FNISHED ALL PROCESSES");
 
         
 
